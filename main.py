0db7a6750c90117f1b08131ebaddc52cc5472d97
vc-k <velicankupeli@gmail.com>
2017-10-28 18:10:26 +0300

    mongodb

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import tweepy
 from tweepy import OAuthHandler
 import json
 from pprint import pprint
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://veli:kamil7insan@ds237445.mlab.com:37445/heroku_wzpvdrhs')
+db = client['heroku_wzpvdrhs']
+collection = db.thy
+
+print(collection)
 
 def store_tweet(tweet):
     json_tweet = json.dumps(tweet)
-    pprint(json_tweet)
-    with open('tweets.json', mode='a', encoding='utf-8') as feedsjson:
-        json.dump(json_tweet, feedsjson)
+    # pprint(json_tweet)
+    collection.insert_one(tweet)
 
 with open('accountInfo.json') as data_file:
     accInfo = json.load(data_file)
