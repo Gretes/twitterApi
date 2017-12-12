3d5880ad94e6a67c5e9dbd4944f4280bf1bcb668
vc-k <velicankupeli@gmail.com>
2017-12-12 17:28:00 +0300

    beinsports

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import tweepy
 from tweepy import OAuthHandler
 import json
 from pymongo import MongoClient
+from time import sleep
+import sys
+from datetime import datetime
+
 
 client = MongoClient('mongodb://veli:kamil7insan@ds237445.mlab.com:37445/heroku_wzpvdrhs')
 db = client['heroku_wzpvdrhs']
-collection = db.digiturk
+collection = db.bein1
 
-print(collection)
+last_item = collection.find_one(sort=[["id", -1]])
+start_date = datetime.strptime(last_item["created_at"], '%a %b %d %H:%M:%S +0000 %Y').strftime("%Y-%m-%d")
+max_id = last_item["id_str"]
+
+print(collection, '\n', start_date, max_id)
 
 def store_tweet(tweet):
-    #json_tweet = json.dumps(tweet)
-    # pprint(json_tweet)
-    collection.insert_one(tweet)
+    try:
+        collection.insert_one(tweet)
+    except Exception as e:
+        exception_name, exception_value = sys.exc_info()[:2]
+        print(exception_name, exception_value)
+    finally:
+        return True
 
 with open('accountInfo.json') as data_file:
     accInfo = json.load(data_file)
@@ -26,13 +38,29 @@ access_token_secret = accInfo["access_token_secret"]
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-query = '#digiturk'
-max_tweets = 10
+query = '@beINSPORTS_TR'
+max_tweets = 100000
+tweets = tweepy.Cursor(api.search, q=query, since=start_date).items(max_tweets)
 
-for tweets in tweepy.Cursor(api.search, q=query).items(max_tweets):
-    store_tweet(tweets._json)
+while True:
+    try:
+        for tweet in tweets:
+            print(tweet.created_at, tweet.user.name)
+            # store_tweet(tweet._json)
+        break
+    except tweepy.TweepError:
+        sleep(60*15)
+        continue
 
+    except IOError:
+        sleep(60*5)
+        continue
+
+    except StopIteration:
+        break
+
+print("Finished!!!")
 
 exit(1)
--- mongoQuery.py
+++ mongoQuery.py
@@ -1,13 +1,16 @@
 from pymongo import MongoClient
+import datetime
 from pprint import pprint
 
 client = MongoClient('mongodb://veli:kamil7insan@ds237445.mlab.com:37445/heroku_wzpvdrhs')
 db = client['heroku_wzpvdrhs']
-collection = db.thy
+collection = db.bein1
 
 # dili turkce olan post sayisi
-print(collection.find({"lang": "tr"}).count())
+# print(collection.find({"lang": "tr"}).count())
 
+created_at = collection.find_one(sort=[["id", -1]])["created_at"]
+print(datetime.datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y').strftime("%Y-%m-%d %H:%M:%S"))
 
-for post in collection.find({"lang": "tr"}):
-    pprint(post)
+# for post in collection.find():
+#     pprint(post)
