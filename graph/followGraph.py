1fc65d560506a7944671ff14e8fd2267c4e3613d
vc-k <velicankupeli@gmail.com>
2018-05-21 17:10:19 +0300

    async

--- graph/followGraph.py
+++ graph/followGraph.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime
 from time import sleep
 import pyley
+import asyncio
 
 import logging
 import logging.handlers
@@ -52,29 +53,34 @@ searched_ids = []
 LOGGER.info('iteration started')
 
 
-def find_followers(name, cayley_client, user_id=None, depth=0):
-    if depth == 0:
+def find_followers(name, cayley_client, user=None, depth=0):
+    futures = []
+    if user is None:
         user = api.get_user(screen_name=name)
-        user_name = user.name
-        user_screen_name = user.screen_name
-        user_id = user.id_str
-        user_followers_count = user.followers_count
-        user_friends_count = user.friends_count
-        user_tweet_count = user.statuses_count
-        user_created_at = str(user.created_at)
-        user_lang = user.lang
-        user_location = user.location
-        user_time_zone = user.time_zone
-        cayley_client.AddQuad(user_id, 'name', user_name)
-        cayley_client.AddQuad(user_id, 'screen_name', user_screen_name)
-        cayley_client.AddQuad(user_id, 'followers_count', user_followers_count)
-        cayley_client.AddQuad(user_id, 'friends_count', user_friends_count)
-        cayley_client.AddQuad(user_id, 'tweet_count', user_tweet_count)
-        cayley_client.AddQuad(user_id, 'created_at', user_created_at)
-        cayley_client.AddQuad(user_id, 'lang', user_lang)
-        cayley_client.AddQuad(user_id, 'location', user_location)
-        cayley_client.AddQuad(user_id, 'time_zone', user_time_zone)
-        cayley_client.AddQuad(user_id, 'user_update_date', str(datetime.now().date()))
+    user_name = user.name
+    user_screen_name = user.screen_name
+    user_id = user.id_str
+    user_followers_count = str(user.followers_count)
+    user_friends_count = str(user.friends_count)
+    user_tweet_count = str(user.statuses_count)
+    user_created_at = str(user.created_at)
+    user_lang = user.lang
+    user_location = user.location
+    user_time_zone = user.time_zone
+    print(user_time_zone)
+    futures.append(cayley_client.AddQuad(user_id, 'name', user_name))
+    futures.append(cayley_client.AddQuad(user_id, 'screen_name', user_screen_name))
+    futures.append(cayley_client.AddQuad(user_id, 'followers_count', user_followers_count))
+    futures.append(cayley_client.AddQuad(user_id, 'friends_count', user_friends_count))
+    futures.append(cayley_client.AddQuad(user_id, 'tweet_count', user_tweet_count))
+    futures.append(cayley_client.AddQuad(user_id, 'created_at', user_created_at))
+    futures.append(cayley_client.AddQuad(user_id, 'lang', user_lang))
+    futures.append(cayley_client.AddQuad(user_id, 'location', user_location))
+    futures.append(cayley_client.AddQuad(user_id, 'time_zone', user_time_zone))
+    futures.append(cayley_client.AddQuad(user_id, 'user_update_date', str(datetime.now().date())))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(futures))
+    # print(futures)
     if depth >= 3:
         LOGGER.info('Max depth reached! ' + name + ' ' + str(depth))
         return
@@ -83,17 +89,11 @@ def find_followers(name, cayley_client, user_id=None, depth=0):
     while True:
         try:
             for page in pages:
+                futures = []
                 for follower in page:
                     follower_name = follower.name
                     follower_screen_name = follower.screen_name
                     follower_id = follower.id_str
-                    follower_follower_count = follower.followers_count
-                    follower_friends_count = follower.friends_count
-                    follower_tweet_count = follower.statuses_count
-                    follower_created_at = str(follower.created_at)
-                    follower_lang = follower.lang
-                    follower_location = follower.location
-                    follower_time_zone = follower.time_zone
 
                     response = client.Send(g.Vertex(follower_id).In('followers_update_date').All()).result['result']
                     if response is not None:
@@ -107,21 +107,16 @@ def find_followers(name, cayley_client, user_id=None, depth=0):
                         LOGGER.info('Skipping already searched name' + follower_screen_name)
                         continue
 
-                    cayley_client.AddQuad(follower_id, 'name', follower_name)
-                    cayley_client.AddQuad(follower_id, 'screen_name', follower_screen_name)
-                    cayley_client.AddQuad(follower_id, 'followers_count', follower_follower_count)
-                    cayley_client.AddQuad(follower_id, 'friends_count', follower_friends_count)
-                    cayley_client.AddQuad(follower_id, 'tweet_count', follower_tweet_count)
-                    cayley_client.AddQuad(follower_id, 'created_at', follower_created_at)
-                    cayley_client.AddQuad(follower_id, 'lang', follower_lang)
-                    cayley_client.AddQuad(follower_id, 'location', follower_location)
-                    cayley_client.AddQuad(follower_id, 'time_zone', follower_time_zone)
-                    cayley_client.AddQuad(follower_id, 'user_update_date', str(datetime.now().date()))
-                    cayley_client.AddQuad(follower_id, 'follows', user_id)
-                    find_followers(follower_name, cayley_client, follower_id, depth=depth)
+                    futures.append(cayley_client.AddQuad(follower_id, 'follows', user_id))
+                    # loop = asyncio.get_event_loop()
+                    # loop.run_until_complete(asyncio.wait(futures))
+                    # print(futures)
+                    find_followers(follower_name, cayley_client, follower, depth=depth)
 
             searched_ids.append(name)
-            cayley_client.AddQuad(str(datetime.now().date()), 'followers_update_date', user_id)
+            futures.append(cayley_client.AddQuad(str(datetime.now().date()), 'followers_update_date', user_id))
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(asyncio.wait(futures))
             break
         except tweepy.TweepError as e:
             LOGGER.error(e)
